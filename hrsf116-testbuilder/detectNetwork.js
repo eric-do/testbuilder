5f0daf5ce85a42c43179287db40391e8e734f3e2
ericdo <ericdo.617@gmail.com>
2019-02-28 08:43:06 -0800

    Test pull from repo

--- hrsf116-testbuilder/detectNetwork.js
+++ hrsf116-testbuilder/detectNetwork.js
@@ -7,24 +7,65 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
+// Note: `cardNumber` will always be a string
+// The Diner's Club network always starts with a 38 or 39 and is 14 digits long
+// The American Express network always starts with a 34 or 37 and is 15 digits long
+// Visa always has a prefix of 4 and a length of 13, 16, or 19.
+// MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
-
+  let prefix = cardNumber.slice(0,2);
+  return getNetwork(cardNumber);
 
 };
 
 var validateCard = function(cardNumber) {
-	// This function takes a string and returns true/false
-	  // True if card is valid
-	  // False if card in invalid
-	// Invalid errors
-	  // Invalid characters in string
-	  // TODO
+  // This function takes a string and returns true/false
+    // True if card is valid
+    // False if card in invalid
+  // Invalid errors
+    // Invalid characters in string
+    // TODO
 
-	
+
 }
 
+var getNetwork = function(cardNumber) {
+  let network = '';
+  let length = cardNumber.length;
+  let dcRegex = /(38|39)/;
+  let amexRegex = /(34|37)/;
+  let visaRegex = /(4)/;
+  let mcRegex = /(51|52|53|54|55)/;
+
+  if (cardNumber.search(dcRegex) === 0 && length === 14) {
+     network = 'Diner\'s Club';
+  }  else if (cardNumber.search(amexRegex) === 0 && length === 15) {
+  	 network = 'American Express';
+  }  else if (cardNumber.search(visaRegex) === 0 && (length === 13 || length === 16 || length === 19)) {
+     network = 'Visa'
+  }  else if (cardNumber.search(mcRegex) === 0 && length === 16) {
+     network = 'MasterCard'
+  }
+  return network;
+}
+
+function assertEqual(actual, expected, testName) {
+  if (actual === expected) {
+    console.log('Passed');
+  } else {
+    console.log('FAILED [' + testName + '] expected ' + expected + ' but got ' + actual);
+  }
+}
+
+detectNetwork('38345678901234');
+detectNetwork('39345678901234');
+detectNetwork('343456789012345');
+detectNetwork('373456789012345');
+detectNetwork('4123456789012');
+detectNetwork('4123456789012345');
+detectNetwork('4123456789012345678');
+detectNetwork('5112345678901234');
+detectNetwork('5212345678901234');
+detectNetwork('5312345678901234');
+detectNetwork('5412345678901234');
+detectNetwork('5512345678901234');
